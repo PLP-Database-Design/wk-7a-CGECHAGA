f354c827c1b3122047ead6aee1fdea650175b122
CGECHAGA <cgechga@gmail.com>
2025-05-04 14:29:44 +0300

    assignment

--- answers.sql
+++ answers.sql
@@ -1,10 +1,11 @@
-
+use salesdb;
 CREATE TABLE ProductDetail(
 OrderID INT,
-CustomerName(100),
+CustomerName varchar(100),
 Products varchar(100)
  );
- INSERT INTO ProductDetail(OrderID, CustomerName,Products)VALUES
+ INSERT INTO ProductDetail(OrderID, CustomerName,Products)
+ VALUES
 (101,'John Doe', 'Laptop'),
 (101,'John Doe', 'Mouse'),
 (102,'Jane Smith', 'Tablet'),
@@ -19,26 +20,26 @@ OrderID INT primary KEY,
 CustomerName varchar(100)
 );
 
-insert into orders(orderID, CustomerName)
+insert into Orders(OrderID, CustomerName)
 values
-(101,'John Doe')
-(102,'Jane Smith')
+(101,'John Doe'),
+(102,'Jane Smith'),
 (103,'Emily Clark');
 
 create table Product(
-orderID INT,
+OrderID INT,
 Product varchar(100),
-quantity int,
-primary key (orderID,Product),
-foreign key (orderID) references orders(orderID)
+Quantity int,
+primary key (OrderID,Product),
+foreign key (OrderID) references Orders(OrderID)
 );
-insert into Product(orderID, Product,quantity)
+insert into Product(OrderID, Product,Quantity)
 values
-(101,'Laptop', 2)
-(101,'Mouse', 1)
-(102,'Tablet', 3)
-(102,'Keyboard', 1)
-(102,'Mouse', 2)
+(101,'Laptop', 2),
+(101,'Mouse', 1),
+(102,'Tablet', 3),
+(102,'Keyboard', 1),
+(102,'Mouse', 2),
 (103,'Phone', 2);
 
 
